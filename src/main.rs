9f540c9fc903a59819519dfda537f66c02a0ed27
Sgeo <sgeoster@gmail.com>
2017-12-23 00:09:21 -0500

    Initial program. Untested.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+extern crate memmem;
+extern crate byteorder;
+
+use std::io::{self, Read, Write};
+use std::fs::File;
+use memmem::{Searcher, TwoWaySearcher};
+use byteorder::{BigEndian, ByteOrder};
+
 fn main() {
-    println!("Hello, world!");
+    print!("What are you searching for?: ");
+    let mut find: String = String::new();
+    io::stdin().read_line(&mut find).expect("Unable to read line!");
+    find.pop(); // Eliminate newline
+    print!("What are you replacing it with?: ");
+    let mut replace: String = String::new();
+    io::stdin().read_line(&mut replace);
+    replace.pop();
+    
+    let change: isize = (replace.as_bytes().len() as isize) - (find.as_bytes().len() as isize);
+    
+    let searcher = TwoWaySearcher::new(find.as_bytes());
+    
+    for filename in std::env::args() {
+        let mut in_data: Vec<u8> = Vec::new();
+        let mut out_data: Vec<u8> = Vec::new();
+        {
+            let mut file = File::open(&filename).expect("Unable to open file for reading!");
+            file.read_to_end(&mut in_data).expect("Unable to read all data in file!");
+        }
+        let mut cur_slice: &[u8] = &in_data;
+        let mut cur_index: usize = 0;
+        while let Some(sub_index) = searcher.search_in(&in_data) {
+            cur_index += sub_index;
+            let (before, middle_after) = cur_slice.split_at(sub_index - 2);
+            let cur_size = BigEndian::read_u16(middle_after);
+            let new_size = ((cur_size as isize) + change) as u16;
+            let mut new_size_buffer: [u8; 2] = [0; 2];
+            BigEndian::write_u16(&mut new_size_buffer, new_size);
+            cur_slice = middle_after.split_at((cur_size as usize) + 2).1;
+            
+            out_data.extend_from_slice(before);
+            out_data.extend_from_slice(&new_size_buffer);
+            out_data.extend_from_slice(replace.as_bytes());
+        }
+        out_data.extend_from_slice(&cur_slice);
+        
+        let mut file = File::create(&filename).expect("Unable to open file for writing!");
+        file.write_all(&out_data).expect("Unable to write result data!");
+    }
 }
